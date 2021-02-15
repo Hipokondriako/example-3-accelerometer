42bae3efe30d5214c9c2c8814c8bb2495cd2238b
Hipokondriako <79016081+Hipokondriako@users.noreply.github.com>
2021-02-15 12:24:28 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,41 @@
-let Count = 0
+input.onButtonPressed(Button.A, function () {
+    Count += -1
+    Mode = Math.abs(Count % 3)
+})
+input.onButtonPressed(Button.B, function () {
+    Count += 1
+    Mode = Math.abs(Count % 3)
+})
 let Mode = 0
+let Count = 0
+Count = 1000
+Mode = 0
 basic.forever(function () {
-    if (Mode == 0) {
-        basic.showString("X")
-        control.waitMicros(1500)
-    }
     while (Mode == 0) {
-        if (input.buttonIsPressed(Button.A)) {
-            Count += -1
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            Count += 1
-        }
-        Mode = Math.abs(Count % 3)
         led.plotBarGraph(
         input.acceleration(Dimension.X) + 1023,
         2046
         )
-    }
-    if (Mode == 1) {
-        basic.showString("Y")
-        control.waitMicros(1500)
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            break;
+        }
     }
     while (Mode == 1) {
-        if (input.buttonIsPressed(Button.A)) {
-            Count += -1
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            Count += 1
-        }
-        Mode = Math.abs(Count % 3)
         led.plotBarGraph(
         input.acceleration(Dimension.Y) + 1023,
         2046
         )
-    }
-    if (Mode == 2) {
-        basic.showString("Z")
-        control.waitMicros(1500)
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            break;
+        }
     }
     while (Mode == 2) {
-        if (input.buttonIsPressed(Button.A)) {
-            Count += -1
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            Count += 1
-        }
-        Mode = Math.abs(Count % 3)
         led.plotBarGraph(
         input.acceleration(Dimension.Z) + 1023,
         2046
         )
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            break;
+        }
     }
-})
-basic.forever(function () {
-    pins.analogWritePin(AnalogPin.P0, (input.acceleration(Dimension.X) + 1023) / 2)
-    pins.analogWritePin(AnalogPin.P1, (input.acceleration(Dimension.Y) + 1023) / 2)
-    pins.analogWritePin(AnalogPin.P2, (input.acceleration(Dimension.Z) + 1023) / 2)
 })
